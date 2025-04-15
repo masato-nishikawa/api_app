9c7dce56922ab20c1f74b63a9618d95a8f149acf
西川雅人 <masato.nishikawa@arteryex.biz>
2025-04-15 14:41:54 +0900

    APIのルートにアクセスしてユーザー情報の取得

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -31,6 +33,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String apiText = 'api通信をしていません。';
+  List<dynamic> data = [];
 
   Future<void> _incrementCounter() async {
     setState(() {
@@ -39,12 +42,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
     try {
       final ipAddress = '192.168.1.62';
-      final url = Uri.parse('http://$ipAddress:3000/');
+      final url = Uri.parse('http://$ipAddress:3000/api/person');
       final response = await http.get(url);
 
       setState(() {
         if (response.statusCode == 200) {
-          apiText = response.body;
+          apiText = '通信が完了しました。';
+          data = jsonDecode(response.body);
         } else {
           apiText = 'エラー: ${response.statusCode}';
         }
@@ -73,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headlineMedium,
               textAlign: TextAlign.center,
             ),
+            PersonList(personInfo: data)
           ],
         ),
       ),
@@ -81,6 +86,37 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'API通信',
         child: const Icon(Icons.swap_vert),
       ),
+    );
+  }
+}
+
+
+
+
+class PersonList extends StatelessWidget {
+  final List<dynamic> personInfo ;
+  const PersonList({super.key, required this.personInfo});
+
+  @override
+  Widget build(BuildContext context) {
+        return ListView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          // 単純にリストとして表示しスクロールはしない
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: personInfo.length,
+          itemBuilder: (context, index) {
+            final person = personInfo[index];
+            return ListTile(
+              title: Column(
+                children: [
+                  Text('ID:${person['id'].toString()}'),
+                  Text('名前:${person['name']}'),
+                  Text('e-mail:${person['email']}'),
+                ],
+              ),
+        );
+      },
     );
   }
 }
